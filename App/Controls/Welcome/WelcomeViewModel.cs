e5272f7b15cd4100750b24f074d148a03ae5c508
Sergey Khromin <s-hromin@mail.ru>
2022-11-10 02:00:20 +0700

    add textbox & combobox in welcome

--- App/Controls/Welcome/WelcomeViewModel.cs
+++ App/Controls/Welcome/WelcomeViewModel.cs
@@ -1,9 +1,40 @@
 using ReactiveUI;
+using Generators;
+using Avalonia;
+using Avalonia.Controls;
+using System;
 
 namespace App.Controls.Welcome
 {
     public class WelcomeViewModel : ReactiveObject
     {
-        public WelcomeViewModel() { }
+        public string CompName
+        {
+            get => _compName;
+            set => this.RaiseAndSetIfChanged(ref _compName, value);
+        }
+        public AverageBehavior Behavior
+        {
+            get => _behavior;
+            set => this.RaiseAndSetIfChanged(ref _behavior, value);
+        }
+        public string N
+        {
+            get => _n;
+            set
+            {
+                Console.WriteLine("ok!");
+                this.RaiseAndSetIfChanged(ref _n, value);
+            }
+        }
+        private string _compName;
+        private AverageBehavior _behavior;
+        private string _n;
+        public WelcomeViewModel()
+        {
+            // var fontComboBox = this.Find<ComboBox>("fontComboBox");
+        }
+
+        public void CreateGen() { }
     }
 }
