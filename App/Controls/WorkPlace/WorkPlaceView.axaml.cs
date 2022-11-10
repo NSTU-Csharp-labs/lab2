4bf872b38fcc84eb937853ad1831014b9fa196d1
danilka108 <danilka108@yandex.ru>
2022-11-10 19:55:46 +0700

    Add opening addition gen dialog

--- App/Controls/WorkPlace/WorkPlaceView.axaml.cs
+++ App/Controls/WorkPlace/WorkPlaceView.axaml.cs
@@ -1,5 +1,13 @@
+using System.Reactive;
+using System.Threading.Tasks;
+using App.Controls.MainWindow;
+using App.Controls.WorkPlace.AdditionGen;
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Controls.Mixins;
 using Avalonia.ReactiveUI;
 using Avalonia.Markup.Xaml;
+using ReactiveUI;
 
 namespace App.Controls.WorkPlace
 {
@@ -7,7 +15,29 @@ namespace App.Controls.WorkPlace
     {
         public WorkPlaceView()
         {
+            this.WhenActivated(d =>
+            {
+                ViewModel!
+                    .ShowAdditionGen
+                    .RegisterHandler(DoShowAdditionGen)
+                    .DisposeWith(d);
+            });
             InitializeComponent();
+        }
+
+        private async Task DoShowAdditionGen(InteractionContext<AdditionGenViewModel, Unit> interactionContext)
+        {
+            var dialog = new AdditionGenWindow
+            {
+                DataContext = interactionContext.Input
+            };
+            
+            if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                await dialog.ShowDialog(desktop.MainWindow);
+            }
+            
+            interactionContext.SetOutput(Unit.Default);
         }
 
         public void InitializeComponent()
--- App/Controls/WorkPlace/WorkPlaceViewModel.cs
+++ App/Controls/WorkPlace/WorkPlaceViewModel.cs
@@ -1,3 +1,7 @@
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using App.Controls.WorkPlace.AdditionGen;
 using Avalonia;
 using Generators;
 using ReactiveUI;
@@ -7,12 +11,23 @@ namespace App.Controls.WorkPlace
     public class WorkPlaceViewModel : ReactiveObject, IRoutableViewModel
     {
         public CompositionGen CompositionGen { get; }
+        public ReactiveCommand<Unit, Unit> AddGenerator { get; }
+            
 
         public WorkPlaceViewModel(IScreen hostScreen)
         {
             HostScreen = hostScreen;
             CompositionGen = CompositionGenManager.Get();
+
+            ShowAdditionGen = new Interaction<AdditionGenViewModel, Unit>();
+            
+            AddGenerator = ReactiveCommand.CreateFromTask(async () =>
+            {
+                await ShowAdditionGen.Handle(new AdditionGenViewModel());
+            });
         }
+
+        public Interaction<AdditionGenViewModel, Unit> ShowAdditionGen;
 
         public object? UsedAverageBehaviour => CompositionGen.AverageBehavior switch
         {
